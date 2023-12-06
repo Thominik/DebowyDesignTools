7b5ad86faf6fc423d8f69c0b0bf90a55bf773b33
Thominik <dominikbednarz.ti@gmail.com>
2023-12-06 22:10:36 +0100

    Load file and user interface

--- DebowyDesignTools/DebowyDesignTools/Program.cs
+++ DebowyDesignTools/DebowyDesignTools/Program.cs
@@ -1,43 +1,71 @@
-﻿using DebowyDesignTools.Data;
-using DebowyDesignTools.Entities;
+﻿using DebowyDesignTools.Entities;
 using DebowyDesignTools.Repositories;
-using DebowyDesignTools.Repositories.Extensions;
 
-var toolRepo = new SqlRepository<Tool>(new DebowyDesignToolsDbContext());
+WelcomeScreenPrint();
+
+LoadLastFile();
+Console.ReadKey();
+
+var toolRepo = new FileRepository<Tool>(CurrentDateTimeString());
 toolRepo.ItemAdded += ToolRepositoryOnItemAdded;
 toolRepo.ItemRemoved += ToolRepositoryOnItemRemoved;
 
-static void ToolRepositoryOnItemAdded(object? sender, Tool tool)
+Console.Clear();
+bool exitProgram = false;
+while (!exitProgram)
 {
-    Console.WriteLine($"{CurrentDateTime()} --- Tool added => {tool.Name}");
-}
+    Menu();
+    string? choice = Console.ReadLine();
 
-static void ToolRepositoryOnItemRemoved(object? sender, Tool tool)
-{
-    Console.WriteLine($"{CurrentDateTime()} --- Tool removed => {tool.Name}");
-}
-
-AddTools(toolRepo);
-RemoveTools(toolRepo);
-PrintToConsole(toolRepo);
-
-static void AddTools(IRepository<Tool> toolRepo)
-{
-    var tools = new[]
+    switch (choice)
     {
-        new PowerTool {Brand = "Makita", Name = "Screwdriver", Model = "DHP450", Battery = "18v"},
-        new Tool {Brand = "Scheppach", Name = "Saw", Model = "DCP900"},
-        new PowerTool {Brand = "Makita", Name = "MillingMachine", Model = "DSC100", Battery = "18v"},
-        new HandTool {Brand = "Fiskars", Name = "Hammer"}
-    };
-    
-    toolRepo.AddBatch(tools);
+        case "1":
+            Console.Clear();
+            AddTool(toolRepo);
+            break;
+        case "2":
+            Console.Clear();
+            PrintToConsole(toolRepo);
+            RemoveTool(toolRepo);
+            break;
+        case "3":
+            DayInputPrint();
+            var day = Console.ReadLine();
+            var fileRepo = new FileRepository<Tool>(day);
+            PrintToConsole(fileRepo);
+            break;
+        case "q":
+            exitProgram = true;
+            break;
+        default:
+            Console.WriteLine("Incorrect selection, please try again.");
+            break;
+    }
 }
 
-static void RemoveTools(IRepository<Tool> toolRepo)
+static void WelcomeScreenPrint()
 {
-    var toolId = toolRepo.GetById(2);
-    toolRepo.Remove(toolId);
+    Console.WriteLine("Welcome to the program for monitoring the inventory of equipment in the DębowyDesign " +
+                      "carpentry shop!");
+    Console.WriteLine("Remember that if you want to exit, type 'q'");
+    Console.WriteLine("Ready? (confirm with any key)");
+    Console.WriteLine();
+}
+
+static void Menu()
+{
+    Console.WriteLine("Select what you want to do (confirm with enter):");
+    Console.WriteLine("1. Add equipment to the warehouse");
+    Console.WriteLine("2. Remove equipment from storage");
+    Console.WriteLine("3. View the stock level for the day");
+    Console.WriteLine();
+}
+
+static void DayInputPrint()
+{
+    Console.Clear();
+    Console.WriteLine("Enter the day for stock (format: YYYY-MM-DD_HH-MM-SS):");
+    Console.WriteLine();
 }
 
 void PrintToConsole(IReadRepository<IEntity> repository)
@@ -50,7 +78,103 @@ void PrintToConsole(IReadRepository<IEntity> repository)
     }
 }
 
+static void AddTool(FileRepository<Tool> toolRepo)
+{
+    Console.WriteLine("Adding a new tool");
+
+    Console.Write("Enter the name: ");
+    string? name = Console.ReadLine();
+
+    Console.Write("Enter the brand: ");
+    string? brand = Console.ReadLine();
+
+    Console.Write("Enter the model ");
+    string? model = Console.ReadLine();
+
+    Console.Write("Is this a power tool? (yes/no): ");
+    bool isPowerTool = Console.ReadLine().ToLower() == "yes";
+
+    Tool tool;
+    if (isPowerTool)
+    {
+        Console.Write("Enter battery type: ");
+        string battery = Console.ReadLine();
+
+        tool = new PowerTool { Brand = brand, Name = name, Model = model, Battery = battery };
+    }
+    else
+    {
+        tool = new Tool { Brand = brand, Name = name, Model = model };
+    }
+
+    toolRepo.Add(tool);
+    toolRepo.Save();
+
+    Console.WriteLine("Tool added successfully.");
+}
+
+static void RemoveTool(FileRepository<Tool> toolRepo)
+{
+    Console.Write("Enter the ID of the tool to remove: ");
+    int id = Convert.ToInt32(Console.ReadLine());
+    var tool = toolRepo.GetById(id);
+    if (tool != null)
+    {
+        toolRepo.Remove(tool);
+        toolRepo.Save();
+        Console.WriteLine("Tool removed successfully.");
+    }
+    else
+    {
+        Console.WriteLine("No tool with this ID was found.");
+    }
+}
+
+static void ToolRepositoryOnItemAdded(object? sender, Tool tool)
+{
+    Console.WriteLine($"{CurrentDateTime()} --- Tool added => {tool.Name}");
+}
+
+static void ToolRepositoryOnItemRemoved(object? sender, Tool tool)
+{
+    Console.WriteLine($"{CurrentDateTime()} --- Tool removed => {tool.Name}");
+}
+
 static DateTime CurrentDateTime()
 {
     return DateTime.Now;
+}
+
+static string CurrentDateTimeString()
+{
+    return DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+}
+
+void LoadLastFile()
+{
+    var directorypath = @"C:\Users\Dudi\Documents\Projekty\DebowyDesignTools\DebowyDesignTools\bin\Debug\net6.0";
+    var latestFile = GetLatestFile(directorypath, "*.json");
+
+    if (latestFile != null)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Last file: {latestFile.Name}");
+        Console.WriteLine();
+        var fileWithoutExtension = Path.GetFileNameWithoutExtension(latestFile.Name);
+        var fileRepo = new FileRepository<Tool>(fileWithoutExtension);
+        PrintToConsole(fileRepo);
+    }
+    else
+    {
+        Console.WriteLine("File not found!");
+    }
+}
+
+static FileInfo GetLatestFile(string directoryPath, string searchPattern)
+{
+    var directoryInfo = new DirectoryInfo(directoryPath);
+
+    return directoryInfo.GetFiles(searchPattern)
+        .OrderByDescending(x => x.LastWriteTime)
+        .FirstOrDefault();
 }
---
+++ DebowyDesignTools/DebowyDesignTools/Repositories/FileRepository.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+using DebowyDesignTools.Entities;
+
+namespace DebowyDesignTools.Repositories;
+
+public class FileRepository<T> : IRepository<T>
+    where T : class, IEntity, new()
+{
+    private readonly List<T> _items = new();
+
+    private string fileName;
+
+    public event EventHandler<T>? ItemAdded;
+    public event EventHandler<T>? ItemRemoved;
+
+    public FileRepository(string day)
+    {
+        fileName = $"{day}.json";
+        Load();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _items.ToList();
+    }
+
+    public T? GetById(int id)
+    {
+        return _items.SingleOrDefault(item => item.Id == id);
+    }
+
+    public void Add(T item)
+    {
+        item.Id = _items.Count + 1;
+        _items.Add(item);
+        ItemAdded?.Invoke(this, item);
+    }
+
+    public void Remove(T item)
+    {
+        _items.Remove(item);
+        ItemRemoved?.Invoke(this, item);
+    }
+
+    public void Save()
+    {
+        var json = JsonSerializer.Serialize<IEnumerable<T>>(_items);
+        File.WriteAllText(fileName, json);
+    }
+
+    public void Load()
+    {
+        if (File.Exists(fileName))
+        {
+            var json = File.ReadAllText(fileName);
+            var items = JsonSerializer.Deserialize<IEnumerable<T>>(json);
+
+            if (items != null)
+            {
+                _items.Clear();
+                _items.AddRange(items);
+            }
+        }
+    }
+}
